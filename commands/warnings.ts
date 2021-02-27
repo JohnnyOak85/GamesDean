b1fb3c86868931fe7b1e626952c713b1c273f2c1
João Carvalho <joao.carvalho@itcparts.com>
2021-02-27 19:21:06 +0000

    Added warn commands.

---
+++ commands/warnings.ts
@@ -0,0 +1,43 @@
+import { Message } from 'discord.js';
+import { checkMember, getUserWarnings, listWarnings } from '../helpers/member.helper';
+
+module.exports = {
+  name: 'warnings',
+  description: "Lists all users with strikes. If provided with a user, it will list that user's strikes",
+  usage: '<user>',
+  moderation: true,
+  execute: async (message: Message): Promise<void> => {
+    try {
+      if (!message.member?.hasPermission('MANAGE_MESSAGES')) {
+        message.channel.send('You do not have permission for this command.');
+        return;
+      }
+
+      if (!message.mentions.members?.array().length) {
+        const reply = await listWarnings();
+
+        message.channel.send(reply);
+        return;
+      }
+
+      for await (const member of message.mentions.members?.array() || []) {
+        try {
+          const warning = checkMember(message.member, member);
+
+          if (warning) {
+            message.channel.send(warning);
+            return;
+          }
+
+          const reply = await getUserWarnings(member);
+
+          message.channel.send(reply);
+        } catch (error) {
+          throw error;
+        }
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+};

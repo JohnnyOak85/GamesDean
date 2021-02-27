4c42269f68e3defb60930207c896b2e7ad69cddb
João Carvalho <joao.carvalho@itcparts.com>
2021-02-27 00:52:43 +0000

    Updated time helper.

--- helpers/time.helper.ts
+++ helpers/time.helper.ts
@@ -1,7 +1,11 @@
 import moment from 'moment';
 
-const getDate = () => {
-  return moment().format('Do MMMM YYYY, h:mm:ss a');
+const getDate = () => moment().format('Do MMMM YYYY, h:mm:ss a');
+
+const getTime = (timeAmount: string) => {
+  const time = parseInt(timeAmount);
+
+  if (time && time > 0 && time < 100 && !isNaN(time)) return time;
 };
 
-export { getDate };
+export { getDate, getTime };

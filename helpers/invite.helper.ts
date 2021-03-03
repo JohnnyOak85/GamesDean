3699d2ecbf6753489ab69d894fca32a6cfffad77
João Carvalho <joao.carvalho@itcparts.com>
2021-03-03 00:08:44 +0000

    Added invite command.

--- helpers/invite.helper.ts
+++ helpers/invite.helper.ts
@@ -1,10 +1,32 @@
-import { Collection, Invite } from 'discord.js';
+import { Guild, GuildChannelManager } from 'discord.js';
 
-const getInvite = (inviteList: Collection<string, Invite> | undefined) => {
+const createInvite = async (channelManager: GuildChannelManager, channelName: string) => {
   try {
-    if (!inviteList) return;
-    
-    const filteredInvite = inviteList.filter((invite) => !invite.temporary);
+    let channel = channelManager.cache.find((guildChannel) => guildChannel.name === channelName);
+    if (!channel) channel = channelManager.cache.find((guildChannel) => guildChannel.name === 'general-chat');
+
+    return channel?.createInvite({ temporary: false });
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**
+ * @description Returns the guild's permanent invite.
+ * @param inviteList
+ */
+const getInvite = async (guild: Guild | null, channelName: string) => {
+  try {
+    if (!guild) return;
+
+    const inviteList = await guild.fetchInvites();
+    if (!inviteList) return await createInvite(guild.channels, channelName);
+
+    const permanentInvite = inviteList.filter((invite) => !invite.temporary);
+    if (!permanentInvite.array().length) return await createInvite(guild.channels, channelName);
+
+    const filteredInvite = permanentInvite.filter((invite) => invite.channel.name === channelName);
+    if (!filteredInvite.array().length) return await createInvite(guild.channels, channelName);
 
     return filteredInvite.array()[0];
   } catch (error) {

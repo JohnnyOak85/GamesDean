80902392a44ae5c8234f82821366b6eaedbcfaf6
João Carvalho <joao.carvalho@itcparts.com>
2021-03-12 21:43:00 +0000

    Added timed events.

--- helpers/utils.helper.ts
+++ helpers/utils.helper.ts
@@ -1,6 +1,10 @@
 // Dependencies
+import { Guild } from 'discord.js';
 import moment, { unitOfTime } from 'moment';
+import { scheduleJob } from 'node-schedule';
 import { createLogger, format, transports } from 'winston';
+import { unmuteUser } from './roles.helper';
+import { getDoc, readDirectory } from './storage.helper';
 
 const logger = createLogger({
   level: 'info',
@@ -48,4 +52,54 @@ const logInfo = (message: string): void => {
   logger.log('info', `${message}\nTime: ${getDate()}`);
 };
 
-export { addTime, getDate, getNumber, logError, logInfo };
+/**
+ * @description Starts timers for repeated tasks.
+ */
+const startTimers = async (guild: Guild) => {
+  // Once per day at midnight check anniversaries.
+  scheduleJob('1 0 * * *', async () => {
+    const userDocs = await readDirectory(guild.id);
+
+    for (const docPath of userDocs) {
+      const user = await getDoc(`${guild.id}/${docPath}`);
+
+      if (!user.anniversary || moment(user.timer).isBefore(moment().format())) return;
+
+      const guildUser = guild.members.cache.get(user._id);
+
+      guild.systemChannel?.send({
+        embed: {
+          color: 'RANDOM',
+          fields: [
+            {
+              name: `It's ${guildUser?.nickname}'s anniversary!`,
+              value: 'Everyone party!'
+            }
+          ],
+          title: 'HAPPY ANNIVERSARY!',
+          thumbnail: {
+            url: guildUser?.user.avatarURL() || ''
+          },
+          url: 'https://www.youtube.com/watch?v=8zgz2xBrvVQ'
+        }
+      });
+    }
+  });
+
+  // Once per 5 seconds check times.
+  scheduleJob('*/5 * * * * *', async () => {
+    const userDocs = await readDirectory(guild.id);
+
+    for (const docPath of userDocs) {
+      const user = await getDoc(`${guild.id}/${docPath}`);
+
+      if (!user.timer || moment(user.timer).isBefore(moment().format())) return;
+
+      const guildUser = guild.members.cache.get(user._id);
+
+      if (guildUser) unmuteUser(guildUser);
+    }
+  });
+};
+
+export { addTime, getDate, getNumber, logError, logInfo, startTimers };
